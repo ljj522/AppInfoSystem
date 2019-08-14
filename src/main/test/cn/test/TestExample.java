b2e3a9995d5a3c0c36030ae47d3cfd8966afe49c
ljj522 <1030600039@qq.com>
2019-08-14 17:23:37 +0800

    自动生成

--- src/main/test/cn/test/TestExample.java
+++ src/main/test/cn/test/TestExample.java
@@ -1,7 +1,5 @@
 package cn.test;
 
-import cn.appsys.dao.InfoMapper;
-import cn.appsys.pojo.Info;
 import cn.appsys.utils.MyBatisUtils;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
@@ -25,6 +23,21 @@ public class TestExample {
         int rows = mapper.insert(info);
         session.commit();
         System.out.println(rows==1?"添加用户成功！":"添加失败！");
+    }
+    @Test
+    public void test1(){
+        //查询用户
+        session = MyBatisUtils.openSession();
+        InfoMapper mapper = session.getMapper(InfoMapper.class);
+        //实例化模板对象
+        InfoExample example = new InfoExample();
+        //添加查询条件
+        example.createCriteria().andApknameLike("%com%");
+        List<Info> list = mapper.selectByExample(example);
+        for(Info u:list){
+            System.out.println(u.getId()+"\t"+u.getSoftwarename());
+        }
+        MyBatisUtils.closeSession(session);
     }
 
 }
